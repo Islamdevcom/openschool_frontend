87f5f74b73ca347e2512791aeb10a1d5f3b90b10
Islambek <isekedanabekovich@gmail.com>
2025-10-28 20:29:34 +0500

    fix: replace localhost in AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   const login = async ({ email, password }) => {
-    const response = await fetch("http://127.0.0.1:8000/auth/login", {
+    const response = await fetch("https://openschoolbackend-production.up.railway.app/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
